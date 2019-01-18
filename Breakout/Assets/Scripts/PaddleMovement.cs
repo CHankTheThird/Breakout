b78512d07fc47d7823ae19f837cf2ed666777da7
Charles Hankins <chankthethird@gmail.com>
2019-01-18 12:04:29 -0800

    Basic Paddle Movement Established

---
+++ Breakout/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Breakout
+{
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class PaddleMovement : MonoBehaviour
+	{
+		[Header("Movement Settings")]
+		[SerializeField] private float m_movementSpeed = 5f;
+
+		private Rigidbody2D paddleBody { get; set; }
+
+		private void Awake()
+		{
+			paddleBody = GetComponent<Rigidbody2D>();
+		}
+
+		public void UpdateMovement(float deltaTime, float horizontalInput)
+		{
+			if (paddleBody == null)
+				return;
+
+			paddleBody.velocity = new Vector2(horizontalInput * m_movementSpeed, 0f);
+		}
+	}
+}
---
+++ Breakout/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Breakout
+{
+	public class PlayerController : MonoBehaviour
+	{
+		[Header("References")]
+		[SerializeField] private PaddleMovement m_paddleMovement;
+
+		private float m_horizontalInput;
+
+		private void Update()
+		{
+			if (m_paddleMovement != null)
+			{
+				float deltaTime = Time.deltaTime;
+
+				ProcessInput();
+
+				m_paddleMovement.UpdateMovement(deltaTime, m_horizontalInput);
+			}
+		}
+
+		private void ProcessInput()
+		{
+			m_horizontalInput = Input.GetAxis("Horizontal");
+		}
+	}
+}
